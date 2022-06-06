2cd85b71afe536f37f31751103e28abfd6026c39
Baibatchinov Alisher <baibaali@fit.cvut.cz>
2022-06-06 07:52:19 +0200

    Exception class documentation

--- Exception.h
+++ Exception.h
@@ -2,9 +2,23 @@
 #include <exception>
 #include <string>
 
+/**
+ * Custom exception class.
+ * Inherited from std::exception
+ */
 class Exception : public std::exception {
     std::string msg;
 public:
+
+    /**
+     * Constructor
+     * @param message - Exception message
+     */
     Exception(std::string message);
+
+    /**
+     * Returns the exception message
+     * @return Exception message
+     */
     std::string what ();
 };
